94d8a5d893e33eecc26c43c4b68289ff390a78b7
thteam47 <thteam47@gmail.com>
2022-10-11 00:22:35 +0700

    update

--- handler/mongo/mongo.go
+++ handler/mongo/mongo.go
@@ -9,7 +9,7 @@ type MongoRepository interface {
 	InsertOne(data interface{}) (string, error)
 	GetOneByAttr(data map[string]string, result interface{}) error
 	UpdatebyId(data interface{}, id string) error
-	UpdateOneByAttr(userId string, data map[string]interface{}) error 
+	UpdateOneByAttr(userId string, data map[string]interface{}) error  
 	DeleteById(id string) error
 	Find(filter interface{}, options *options.FindOptions) (cur *mongo.Cursor, err error)
 }
